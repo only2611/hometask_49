69286c242133e44e114cd00968ba3cba61d72230
abaibaiyshbekov <abay95poet@inbox.ru>
2022-07-12 13:52:11 +0600

    добавил возможность редактирования через view

--- sourse/trecker/urls.py
+++ sourse/trecker/urls.py
@@ -2,14 +2,14 @@ from django.urls import path
 
 
 
-from trecker.views import IndexView, TaskView, CreateView
+from trecker.views import IndexView, TaskView, CreateView, Update
 
 urlpatterns = [
     path('', IndexView.as_view(), name="index_view"),
     path('task/<int:pk>/', TaskView.as_view(), name="task_view"),
-    path('create/', CreateView.as_view(), name="create")
+    path('create/', CreateView.as_view(), name="create"),
+    path('task/<int:pk>/update', Update.as_view(), name="update"),
 
 ]
 
-#     path('task/<int:pk>/update', update_note, name="update_note"),
 #     path('task/<int:pk>/delete', delete, name="delete"),
--- sourse/trecker/views.py
+++ sourse/trecker/views.py
@@ -49,3 +49,39 @@ class CreateView(View):
                 new_task = Task.objects.create(summary=summary, description=description, status=status, type=type)
                 return redirect("index_view", )
             return render(request, "create.html", {"form": form})
+
+
+class Update(View):
+    def dispatch(self, request, *args, **kwargs):
+        pk = kwargs.get("pk")
+        self.task = get_object_or_404(Task, pk=pk)
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        if request.method == "GET":
+            form = TaskForm(initial={
+                "summary": self.task.summary,
+                "description": self.task.description,
+                "status": self.task.status,
+                "type": self.task.type,
+            })
+            return render(request, "update.html", {"form": form})
+    def post(self, request, *args, **kwargs):
+        form = TaskForm(data=request.POST)
+        if form.is_valid():
+            self.task.summary = form.cleaned_data.get("summary")
+            self.task.description = form.cleaned_data.get("description")
+            self.task.status = form.cleaned_data.get("status")
+            self.task.type = form.cleaned_data.get("type")
+            self.task.save()
+            return redirect("index_view", )
+        return render(request, "update.html", {"form": form})
+
+
+# def delete(request, pk):
+#     note = get_object_or_404(Task, pk=pk)
+#     if request.method == "GET":
+#         return render(request, "delete.html", {"note": note})
+#     else:
+#         note.delete()
+#         return redirect("index_view")
